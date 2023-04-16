45ee2197593acf48cbc89404c90fd3ca20e64733
Muhammad Rafa Eka Pramoedya <91171858+TheR4V3@users.noreply.github.com>
2023-04-16 23:20:03 +0700

    Menambahkan API Mahasiswa

---
+++ tpmodul9_1302210123/Controllers/MahasiswaCon.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace tpmodul9_1302210123.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaCon : ControllerBase
+    {
+        public static List<Mahasiswa> mh = new List<Mahasiswa>
+        {
+            new Mahasiswa ("Rafa Eka", "1302210123"),
+            new Mahasiswa ("Andry Nur Falah", "1302213005"),
+            new Mahasiswa ("Reyhan Azani", "1302210110"),
+            new Mahasiswa ("Satrio Agul", "1302210065"),
+            new Mahasiswa ("Yosua William", "1302210126")
+        };
+
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get()
+        {
+            return mh;
+        }
+
+        [HttpGet("{idx}")]
+        public Mahasiswa Get(int idx)
+        {
+            return mh[idx];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa newMahasiswa)
+        {
+            mh.Add(newMahasiswa);
+        }
+
+        [HttpPut("{idx}")]
+        public void Put(int idx, [FromBody] Mahasiswa updatedMahasiswa)
+        {
+            mh[idx] = updatedMahasiswa;
+        }
+
+        [HttpDelete("{idx}")]
+        public void Delete(int idx)
+        {
+            mh.RemoveAt(idx);
+        }
+    }
+}
